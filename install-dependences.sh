e83d6bc538568451c0f42fc00fbf35d709cac567
Alex Gamas <alex@gamas.com.br>
2015-03-27 11:27:16 -0300

    run with sudo.

--- install-dependences.sh
+++ install-dependences.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-sudo aptitude install python-pip
-sudo pip install sqlalchemy
-sudo pip install flask-sqlalchemy
-sudo pip install Flask-WTF
-sudo pip install gunicorn
-sudo pip install flask-restful
+aptitude install python-pip
+
+pip install sqlalchemy
+pip install flask-sqlalchemy
+pip install Flask-WTF
+pip install gunicorn
+pip install flask-restful
 
 pip freeze > dependences_list
 
